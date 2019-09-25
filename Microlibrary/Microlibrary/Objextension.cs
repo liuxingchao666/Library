7d3abfeddc595970c16b4996b16723489106663a
liuxingchao666 <1069461287@qq.com>
2019-09-25 11:00:02 +0800

    预

--- Microlibrary/Microlibrary/Objextension.cs
+++ Microlibrary/Microlibrary/Objextension.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace Microlibrary
 {
@@ -49,6 +51,62 @@ namespace Microlibrary
                 return result;
             }
             return null;
+        }
+        /// <summary>
+        /// 接受数据流转图片显示
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static BitmapImage ToBitmapImageByByte(this byte[] data)
+        {
+            if (data == null)
+                return null;
+            try
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                using (MemoryStream memoryStream = new MemoryStream(data))
+                {
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad; // here
+                    bitmapImage.StreamSource = memoryStream;
+                    bitmapImage.EndInit();
+                    bitmapImage.Freeze();
+                }
+                return bitmapImage;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// 本地显示图片转byte[] 数组
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static byte[] ToByteByBitmapImage(this BitmapImage obj)
+        {
+            if (obj == null)
+                return null;
+            byte[] bytearray = null;
+            try
+            {
+                Stream smarket = obj.StreamSource; ;
+                if (smarket != null && smarket.Length > 0)
+                {
+                    //设置当前位置
+                    smarket.Position = 0;
+                    using (BinaryReader br = new BinaryReader(smarket))
+                    {
+                        bytearray = br.ReadBytes((int)smarket.Length);
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            return bytearray;
         }
     }
 }
